1f07ce4820032cb90d3b7e71d958e32f0780c7ab
Pavel Schoffer <pavel.schoffer@ntm.eu>
2020-01-21 15:25:57 +0100

    good stuff

--- selenium/index.js
+++ selenium/index.js
@@ -1,25 +1,31 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
 
-const startURL =
-  'https://speedcoding.toptal.com/challenge?ch=toptal-speedcoding';
-
-chrome.setDefaultService(
-  new chrome.ServiceBuilder(
-    './node_modules/chromedriver/bin/chromedriver'
-  ).build()
-);
-
-(async function example() {
-  console.log('Gonna start!');
-  try {
-    let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get(startURL);
-    title = await driver.getTitle();
-    console.log('Filling form ' + title);
-  } catch (e) {
-    console.log('ERRORR', e);
-  } finally {
-    await driver.quit();
+function solution(x) {
+  function matches(a, b) {
+    var matchItems = a.map((aItem, ix) => aItem === b[ix])
+    return !matchItems.includes(false)
   }
-})();
+  var sorted = x.concat().sort((a, b) => a < b ? -1 : 1)
+  var reversed = x.concat().reverse()
+  return matches(x, sorted) ? 1 : (matches(reversed, sorted) ? -1 : 0)
+}
+// report_problemsortingType( [0,-1,-100] ) = 0expected: -1
+
+// report_problemsortingType( [-2,4,10,19] ) = 0expected: 1
+
+// report_problemsortingType( [1,11,101] ) = 0expected: 1
+
+// report_problemsortingType( [101,11,1] ) = 0expected: -1
+
+// donesortingType( [14,-2,1.5] ) = 0
+
+// donesortingType( [18,18.1,19] ) = 1
+
+// report_problemsortingType( [0.9,0.4,-0.1,-0.12,-1] ) = 0expected: -1
+// x is an array of at least 2 unique members
+// return 0 if it's not sorted, 1 if it's ascending, -1 if it's descending
+
+
+x = [101, 11, 1]
+result = solution(x)
+
+console.log(result)
---
+++ selenium/python.py
@@ -0,0 +1,103 @@
+# Simple assignment
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+startURL = "https://speedcoding.toptal.com/challenge?ch=toptal-speedcoding"
+
+currentQuestionNu = 1
+
+
+def getTextarea(number):
+    while True:
+        print("looking for ", number)
+        try:
+            questionField = driver.find_element_by_class_name("current-task-number")
+
+            if str(number) in questionField.text:
+                textarea = driver.find_element_by_tag_name("textarea")
+                return textarea
+        except:
+            pass
+        time.sleep(0.5)
+
+
+def getQuestion():
+    return driver.find_element_by_class_name("ace_function").text
+
+
+def confirm(textarea):
+    while True:
+        print("Waiting for gree checks")
+        try:
+            driver.find_element_by_class_name("text-success")
+            ActionChains(driver).key_down(Keys.CONTROL, textarea).perform()
+            ActionChains(driver).key_down(Keys.RETURN, textarea).perform()
+            return
+        except:
+            ActionChains(driver).key_down(Keys.CONTROL, textarea).perform()
+            ActionChains(driver).key_down(Keys.RETURN, textarea).perform()
+            ActionChains(driver).key_up(Keys.CONTROL, textarea).perform()
+            ActionChains(driver).key_up(Keys.RETURN, textarea).perform()
+
+
+# 43	Pavel Schoffer	425
+
+results = {
+    "numberToString": 'return x + ""',
+    "double": "return x*2",
+    "floatToInt": "return x < 0 ? Math.ceil(x) : Math.floor(x)",
+    "square": "return x*x",
+    "isEven": "return x % 2 == 0",
+    "squareroot": "return Math.sqrt(x)",
+    "removeFirstFive": "return x.slice(5)",
+    "reverseString": 'return x.split("").reverse().join("")',
+    "stringToNumber": "return Number(x.replace(/[^0-9-.]/g,''))",
+    "swapHalves": "all = x.split('')\nhalf = all.length / 2\nreturn all.slice(half).join('') + all.slice(0, half).join('')",
+    "sumDigits": "cifers = x.toString().split('')\nreturn cifers.reduce((sum, other) => { return ['-', '.'].includes(other) ? sum : sum + Number(other) }, 0)",
+    "removeLastFive": "return x.slice(0,-5)",
+    "dateRank": 'return Math.round((new Date(x) - new Date("12/31/2018")) / (1000 * 3600 * 24))',
+    "oddElements": "var result = []\nfor (var i = 0; i < x.length; i = i + 2) result.push(x[i])\nreturn result",
+    "getFileExtension": "var parts = x.split('.')\nreturn parts.length > 1 ? parts[parts.length - 1] : ''",
+    "doubleIndex": "return x.filter((item, ix) => { return item == ix * 2 })",
+    "longestString": "return x.sort((a, b) => a.length > b.length ? -1 : 1)[0]",
+    "hasOnlyVowels": "return x.split('').filter(l => ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'].includes(l)).length === x.length",
+    "monthFromUnixTimestamp": "return (new Date(x * 1000)).getMonth() + 1",
+    "flatten": "return x.flat(Infinity)",
+    "invertCase": "return x.split('').map(l => l.toUpperCase() === l ? l.toLowerCase() : l.toUpperCase()).join('')",
+    "sortingType": "function matches(a, b) {\nvar matchItems = a.map((aItem, ix) => aItem == b[ix])\nreturn !matchItems.includes(false)\n}\nvar sorted = x.concat().sort((a, b) => a < b ? -1 : 1)\nreturn matches(x, sorted) ? 1 : (matches(x.reverse(), sorted) ? -1 : 0)",
+    "isBalanced": "var parans = x.split('').filter(c => ['(', ')'].includes(c))\nvar open = 0\nvar broken = false\nparans.forEach(paran => {\nif (paran === '(') open++\nelse {\nbroken = broken || open < 1;\nopen--\n}});\nreturn !broken && open == 0",
+}
+
+
+print("Starting")
+driver = Chrome()
+driver.get(startURL)
+
+print("Went to toptal")
+
+driver.find_element_by_id("leaderboardName").send_keys("Pavel Schoffer")
+driver.find_element_by_id("email").send_keys("pavel.schoffer@toptal.com")
+
+driver.find_element_by_tag_name("button").click()
+
+print("Let's do this")
+
+textarea = getTextarea(currentQuestionNu)
+question = getQuestion()
+currentQuestionNu = currentQuestionNu + 1
+print("Doing: ", question)
+while question in results:
+    answer = results[question]
+    print("Answering: ", answer)
+    textarea.send_keys(answer)
+
+    confirm(textarea)
+
+    textarea = getTextarea(currentQuestionNu)
+    question = getQuestion()
+    currentQuestionNu = currentQuestionNu + 1
+
+print("Unknown!")
+
