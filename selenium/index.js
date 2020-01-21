688b20786cb35383f3fcea08c62ebf1d3f4722f6
Pavel Schoffer <xschof00@gmail.com>
2020-01-21 10:24:41 +0100

    test

--- selenium/index.js
+++ selenium/index.js
@@ -1,15 +1,24 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
 const startURL =
   'https://speedcoding.toptal.com/challenge?ch=toptal-speedcoding';
 
+chrome.setDefaultService(
+  new chrome.ServiceBuilder(
+    './node_modules/chromedriver/bin/chromedriver'
+  ).build()
+);
+
 (async function example() {
   console.log('Gonna start!');
-  let driver = await new Builder().forBrowser('firefox').build();
   try {
+    let driver = await new Builder().forBrowser('chrome').build();
     await driver.get(startURL);
     title = await driver.getTitle();
     console.log('Filling form ' + title);
+  } catch (e) {
+    console.log('ERRORR', e);
   } finally {
     await driver.quit();
   }
