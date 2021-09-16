db18107c283282b071ca2106653a4917d5123a31
Pavel Schoffer <xschof00@gmail.com>
2021-09-16 06:40:33 +0200

    more banner

---
+++ mariaDB/README.md
@@ -0,0 +1,7 @@
+# MariaDB / MysqlDB test
+
+Creates `test_db` on startup. Credentials are `root:example`.
+
+```
+mysql -h 127.0.0.1 -u root -p -D test_db
+```
--- meetups/banner/src/App.js
+++ meetups/banner/src/App.js
@@ -1,16 +1,18 @@
 import logo from './simplycruit.png';
 import './App.css';
 import background from './background.jpg'
+import gitlab from './gitlab.png'
 
 function App() {
   return (
-    <div className="background" style={{ background: `url(${background})` }}>
+    <div className="background" style={{ backgroundImage: `url(${background})` }}>
       <div className="backdrop">
         <h2>How Is It Built:</h2>
         <div className="border">
           <img className="logo" src={logo} />
         </div>
       </div>
+      <img className="gitlab" src={gitlab} />
     </div>
   );
 }
