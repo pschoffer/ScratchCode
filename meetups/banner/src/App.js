ea3c47a52d84cf6a237167963a933d2d5d200f0c
Pavel Schoffer <xschof00@gmail.com>
2021-11-21 19:57:28 +0100

    stuff

--- meetups/banner/src/App.js
+++ meetups/banner/src/App.js
@@ -2,16 +2,21 @@ import logo from './simplycruit.png';
 import './App.css';
 import background from './background.jpg'
 import gitlab from './gitlab.png'
+import qr from './qr.png'
 
 function App() {
   return (
     <div className="background" style={{ backgroundImage: `url(${background})` }}>
       <div className="backdrop">
-        <h2>How to Build:</h2>
+       <h3>Meetup Group</h3>
+       <h2>Norrköping Software Development</h2>
+{/*
         <div className="border">
           <h1>Fyne</h1>
           <p>Graphic User Interface Library</p>
-        </div>
+        </div>  */}
+
+        <img src={qr}/>
       </div>
     </div>
   );
